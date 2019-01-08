bf9449c5d9c897c6fbfdeaad37f01f9413750ba7
Selim Dincer <wowselim@live.de>
2019-01-08 23:44:56 +0100

    1.0: finalize api, cleanup implementation

--- src/main/java/co/selim/thumbnail4j/ThumbnailCreator.java
+++ src/main/java/co/selim/thumbnail4j/ThumbnailCreator.java
@@ -1,50 +1,42 @@
 package co.selim.thumbnail4j;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URI;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 
 /**
  * Utility class that can be used to create thumbnail images.
  */
 public class ThumbnailCreator {
-    private static final List<String> THUMBNAIL_COMMANDS = Arrays.asList("convert", "-thumbnail");
-    private static final ProcessBuilder processBuilder = new ProcessBuilder();
+    private static final List<String> THUMBNAIL_COMMANDS = List.of("convert", "-thumbnail");
 
     /**
-     * Returns a {@link CompletableFuture} that, on completion, returns a byte array representing the thumbnail image.
+     * Returns a byte array representing the thumbnail image.
+     * This method calls the command line utility 'convert' to create a thumbnail image of the specified dimension.
      * The original aspect ratio will be preserved.
      *
      * @param px      number of pixels on the longest side
-     * @param fileUrl url pointing to a local file
-     * @return a future that, on completion, returns a byte array representing the thumbnail image.
+     * @param fileUri uri pointing to a local file
+     * @throws IOException if an IO related error occurs
      */
-    public static CompletableFuture<byte[]> createThumbnail(int px, URL fileUrl) {
-        return CompletableFuture.supplyAsync(() -> {
-            ByteArrayOutputStream thumbnailBytes = new ByteArrayOutputStream();
-            try {
-                List<String> command = new ArrayList<>(THUMBNAIL_COMMANDS);
-                command.add(String.valueOf(px));
-                command.add(Paths.get(fileUrl.toURI()).toString());
-                command.add("-");
-                Process convertProcess = processBuilder.command(command)
-                        .redirectError(ProcessBuilder.Redirect.INHERIT)
-                        .start();
+    public byte[] createThumbnail(int px, URI fileUri) throws IOException {
+        ByteArrayOutputStream thumbnailBytes = new ByteArrayOutputStream();
+        List<String> command = new ArrayList<>(THUMBNAIL_COMMANDS);
+        Collections.addAll(command, String.valueOf(px), Paths.get(fileUri).toString(), "-");
 
-                try (InputStream processInputStream = convertProcess.getInputStream()) {
-                    processInputStream.transferTo(thumbnailBytes);
-                }
+        Process convertProcess = new ProcessBuilder(command)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start();
 
-                return thumbnailBytes.toByteArray();
-            } catch (Exception e) {
-                throw new CompletionException(e);
-            }
-        });
+        try (InputStream processInputStream = convertProcess.getInputStream()) {
+            processInputStream.transferTo(thumbnailBytes);
+        }
+
+        return thumbnailBytes.toByteArray();
     }
 }
