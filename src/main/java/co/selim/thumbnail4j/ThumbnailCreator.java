b844a453fa78bd3c62f968e04902e02fc9b891d7
Selim Dincer <wowselim@live.de>
2019-01-15 20:21:17 +0100

    1.1: add option to create thumbnails from inputstreams

--- src/main/java/co/selim/thumbnail4j/ThumbnailCreator.java
+++ src/main/java/co/selim/thumbnail4j/ThumbnailCreator.java
@@ -3,40 +3,73 @@ package co.selim.thumbnail4j;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
  * Utility class that can be used to create thumbnail images.
  */
 public class ThumbnailCreator {
-    private static final List<String> THUMBNAIL_COMMANDS = List.of("convert", "-thumbnail");
+    /**
+     * Returns a byte array representing the thumbnail image.
+     * This method calls the command line utility 'convert' to create a thumbnail image of the specified dimension.
+     * The original aspect ratio will be preserved.
+     *
+     * @param px       number of pixels on the longest side
+     * @param imageUri uri pointing to a local image
+     * @return a byte array representing the thumbnail image
+     * @throws IOException if an IO related error occurs
+     */
+    public static byte[] createThumbnail(int px, URI imageUri) throws IOException {
+        ByteArrayOutputStream thumbnailBytes = new ByteArrayOutputStream();
+        List<String> command = List.of("convert",
+                "-thumbnail",
+                String.valueOf(px),
+                Paths.get(imageUri).toString(),
+                "-");
+
+        Process convertProcess = new ProcessBuilder(command)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start();
+
+        copyAndClose(convertProcess.getInputStream(), thumbnailBytes);
+
+        return thumbnailBytes.toByteArray();
+    }
 
     /**
      * Returns a byte array representing the thumbnail image.
      * This method calls the command line utility 'convert' to create a thumbnail image of the specified dimension.
      * The original aspect ratio will be preserved.
      *
-     * @param px      number of pixels on the longest side
-     * @param fileUri uri pointing to a local file
+     * @param px               number of pixels on the longest side
+     * @param imageInputStream input stream containing the original image
+     * @return a byte array representing the thumbnail image
      * @throws IOException if an IO related error occurs
      */
-    public byte[] createThumbnail(int px, URI fileUri) throws IOException {
+    public static byte[] createThumbnail(int px, InputStream imageInputStream) throws IOException {
         ByteArrayOutputStream thumbnailBytes = new ByteArrayOutputStream();
-        List<String> command = new ArrayList<>(THUMBNAIL_COMMANDS);
-        Collections.addAll(command, String.valueOf(px), Paths.get(fileUri).toString(), "-");
+        List<String> command = List.of("convert",
+                "-thumbnail",
+                String.valueOf(px),
+                "-",
+                "-");
 
         Process convertProcess = new ProcessBuilder(command)
                 .redirectError(ProcessBuilder.Redirect.INHERIT)
                 .start();
 
-        try (InputStream processInputStream = convertProcess.getInputStream()) {
-            processInputStream.transferTo(thumbnailBytes);
-        }
+        copyAndClose(imageInputStream, convertProcess.getOutputStream());
+        copyAndClose(convertProcess.getInputStream(), thumbnailBytes);
 
         return thumbnailBytes.toByteArray();
+    }
+
+    private static void copyAndClose(InputStream from, OutputStream to) throws IOException {
+        try (from; to) {
+            from.transferTo(to);
+        }
     }
 }
