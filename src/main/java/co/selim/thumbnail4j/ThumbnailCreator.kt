9b6e8260dd1ca275423b1d30d5c5c2719babbf45
Selim Dincer <wowselim@live.de>
2019-04-19 10:32:15 +0200

    add javadocs

--- src/main/java/co/selim/thumbnail4j/ThumbnailCreator.kt
+++ src/main/java/co/selim/thumbnail4j/ThumbnailCreator.kt
@@ -2,6 +2,9 @@ package co.selim.thumbnail4j
 
 import java.io.InputStream
 
+/**
+ * Creates a thumbnail image of the given size. The aspect ratio will be preserved.
+ */
 fun InputStream.createThumbnail(px: Int): ByteArray {
     val command = listOf("convert",
             "-thumbnail",
